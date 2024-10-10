29cd29b8b39e1f0d82f662fdc3dc5b8c23ef967c
Samicowest <samicowest@gmail.com>
2024-10-10 11:47:23 +0100

    half ot the easybank

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "hsl(233, 26%, 24%)",
+        limeGreen: "hsl(136, 65%, 51%)",
+        brightCyan: "hsl(192, 70%, 51%)",
+        grayBlue: "hsl(233, 8%, 62%)",
+        lightGrayb: "hsl(220, 16%, 96%)",
+        veryLightG: "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        sans: ["Public Sans", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
